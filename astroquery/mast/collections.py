4191544a300a3dadaa0a4e94aaacbcdb8a27206d
Jennifer Medina <jmedina@stsci.edu>
2022-02-03 16:46:06 -0500

    plato service is not case sensitive

--- astroquery/mast/collections.py
+++ astroquery/mast/collections.py
@@ -44,8 +44,7 @@ class CatalogsClass(MastQueryWithLogin):
                                   "args": {"data_release": "dr2", "table": "mean"}},
                     "plato": {"path": "plato/{data_release}/{table}.json",
                                          "args": {"data_release": "dr2", "table": "mean"}}}
-        
-        print(services)
+
         self._service_api_connection.set_service_params(services, "catalogs", True)
 
         self.catalog_limit = None
